f4f3189be0c96369b0fdcc75f2ffcba7a707eb53
Tej Magar <38379046+tejmagar@users.noreply.github.com>
2022-08-10 08:38:59 +0545

    Update main.py

--- main.py
+++ main.py
@@ -11,35 +11,35 @@ from keras.preprocessing.image import ImageDataGenerator
 train = ImageDataGenerator(rescale=1 / 255)
 validation = ImageDataGenerator(rescale=1 / 255)
 
-# train_dataset = train.flow_from_directory('train/', target_size=(200, 200), batch_size=8, class_mode='binary')
-# print(train_dataset.classes)
-#
-# validation_dataset = train.flow_from_directory('validation/', target_size=(200, 200), batch_size=8, class_mode='binary')
-# print(train_dataset.classes)
-#
-# model = tf.keras.models.Sequential(
-#     [
-#         tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(200, 200, 3)),
-#         tf.keras.layers.MaxPool2D(2, 2),
-#
-#         tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-#         tf.keras.layers.MaxPool2D(2, 2),
-#
-#         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-#         tf.keras.layers.MaxPool2D(2, 2),
-#
-#         tf.keras.layers.Flatten(),
-#
-#         tf.keras.layers.Dense(512, activation='relu'),
-#         tf.keras.layers.Dense(1, activation='sigmoid')
-#     ]
-# )
-#
-# model.compile(loss='binary_crossentropy', optimizer=RMSprop(rho=0.001), metrics=['accuracy'])
-#
-# model_fit = model.fit(train_dataset, steps_per_epoch=2, epochs=32, validation_data=validation_dataset)
-#
-# model.save('model.h5', include_optimizer=False)
+train_dataset = train.flow_from_directory('train/', target_size=(200, 200), batch_size=8, class_mode='binary')
+print(train_dataset.classes)
+
+validation_dataset = train.flow_from_directory('validation/', target_size=(200, 200), batch_size=8, class_mode='binary')
+print(train_dataset.classes)
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(200, 200, 3)),
+        tf.keras.layers.MaxPool2D(2, 2),
+
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D(2, 2),
+
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D(2, 2),
+
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ]
+)
+
+model.compile(loss='binary_crossentropy', optimizer=RMSprop(rho=0.001), metrics=['accuracy'])
+
+model_fit = model.fit(train_dataset, steps_per_epoch=2, epochs=32, validation_data=validation_dataset)
+
+model.save('model.h5', include_optimizer=False)
 
 model2 = tf.keras.models.load_model(
     'model.h5', custom_objects=None, compile=False, options=None
